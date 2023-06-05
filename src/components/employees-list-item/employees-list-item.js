745955a3b38359f097773f07067c931b158ab572
Konstantin0 <maschkovc@yandex.ru>
2023-06-05 16:54:15 +0300

    scss в react

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -2,37 +2,40 @@ import './employees-list-item.css';
 
 
 const EmployeesListItem = (props) => {
-        const { name, salary, onDelete, onToggleIncrease, onTogleLike, increase, like } = props;
+    const { name, salary, onDelete, onToggleIncrease, onTogleLike, increase, like } = props;
 
-        let classNameLi = "list-group-item d-flex justify-content-between";
-        if (increase) {
-            classNameLi += " increase"
-        }
-        if (like) {
-            classNameLi += " like"
-        }
-
-        return (
-            <li className={classNameLi}>
-                <span className="list-group-item-label" onClick={onTogleLike}>{name}</span>
-                <input type="text" className="list-group-item-input" defaultValue={salary + " $"} />
-                <div className='d-flex justify-content-center align-items-center'>
-                    <button type="button"
-                        className="btn-cookie btn-sm "
-                        onClick={onToggleIncrease}>
-                        <i className="fas fa-cookie"></i>
-                    </button>
-
-                    <button type="button"
-                        className="btn-trash btn-sm "
-                        onClick={onDelete}>
-                        <i className="fas fa-trash"></i>
-                    </button>
-                    <i className="fas fa-star"></i>
-                </div>
-            </li>
-        )
+    let classNameLi = "list-group-item d-flex justify-content-between";
+    if (increase) {
+        classNameLi += " increase"
     }
+    if (like) {
+        classNameLi += " like"
+    }
+
+    return (
+        <li className={classNameLi}>
+            <span
+                className="list-group-item-label"
+                onClick={onTogleLike}
+            >{name}</span>
+            <input type="text" className="list-group-item-input" defaultValue={salary + " $"} />
+            <div className='d-flex justify-content-center align-items-center'>
+                <button type="button"
+                    className="btn-cookie btn-sm "
+                    onClick={onToggleIncrease}>
+                    <i className="fas fa-cookie"></i>
+                </button>
+
+                <button type="button"
+                    className="btn-trash btn-sm "
+                    onClick={onDelete}>
+                    <i className="fas fa-trash"></i>
+                </button>
+                <i className="fas fa-star"></i>
+            </div>
+        </li>
+    )
+}
 
 
 export default EmployeesListItem;
