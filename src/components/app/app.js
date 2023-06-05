c9aba8717f7b551e9d5b13cf986be552c073fd49
Konstantin0 <maschkovc@yandex.ru>
2023-06-05 15:54:35 +0300

    фильтры

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,6 +17,8 @@ class App extends Component {
 				{ name: "Oleg S.", salary: 1000, increase: false, like: false, id: 2 },
 				{ name: "Konstantin M.", salary: 1500, increase: false, like: true, id: 3 },
 			],
+			term: '',
+			filter: 'all',
 		}
 		this.maxId = 4
 	}
@@ -65,24 +67,53 @@ class App extends Component {
 		}))
 	}
 
+	searchEmp = (items, term) => {
+		if (term.length === 0) {
+			return items;
+		}
+		return items.filter(item => {
+			return item.name.indexOf(term) > -1
+		})
+	}
+
+	onUpdateSearch = (term) => {
+		this.setState({ term: term })
+	}
+
+	filterPost = (item, filter) => {
+		switch (filter) {
+			case "like":
+				return item.filter(item => item.like);
+			case "salary":
+				return item.filter(item => item.salary > 1000);
+			default: return item;
+		}
+	}
+
+	onFilterSelect = (filter) => {
+		this.setState({ filter: filter })
+	}
 
 	render() {
+		const { data, term, filter } = this.state;
 		const employees = this.state.data.length;
 		const increased = this.state.data.filter(item => item.increase).length;
+		const visibleData = this.filterPost(this.searchEmp(data, term), filter);
+
 		return (
 			<div className="app">
-				<AppInfo 
-				employees={employees} 
-				increased={increased} 
+				<AppInfo
+					employees={employees}
+					increased={increased}
 				/>
 
 				<div className="search-panel">
-					<SearchPanel />
-					<AppFilter />
+					<SearchPanel onUpdateSearch={this.onUpdateSearch} />
+					<AppFilter filter={filter} onFilterSelect={this.onFilterSelect} />
 				</div>
 
 				<EmployeesList
-					data={this.state.data}
+					data={visibleData}
 					onDelete={this.deleteItem}
 					onToggleIncrease={this.onToggleIncrease}
 					onTogleLike={this.onTogleLike} />
