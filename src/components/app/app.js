e3c4e8e1d1e3fea2bf7a19c38da8a3e58937007a
Konstantin0 <maschkovc@yandex.ru>
2023-06-05 11:40:04 +0300

    добаавление/удаление - элементов

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,29 +4,64 @@ import AppFilter from '../app-filter/app-filter';
 import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
+import { Component } from 'react/cjs/react.development';
+
 import './app.css';
 
-function App() {
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: [
+				{ name: "Egor T.", salary: 500, increase: false, like: false, id: 1 },
+				{ name: "Oleg S.", salary: 1000, increase: false, like: false, id: 2 },
+				{ name: "Konstantin M.", salary: 1500, increase: true, like: false, id: 3 },
+			],
+		}
+		this.maxId = 4
+	}
 
-	const data = [
-		{ name: "Egor T.", salary: 500, increase: false, like: false, id: 1 },
-		{ name: "Oleg S.", salary: 1000, increase: false, like: false, id: 2 },
-		{ name: "Konstantin M.", salary: 1500, increase: true, like: false, id: 3 },
-	];
+	deleteItem = (id) => {
+		this.setState(({ data }) => {
+			return {
+				data: data.filter(item => item.id !== id)
+			}
+		})
+	}
 
-	return (
-		<div className="app">
-			<AppInfo />
+	addItem = (name, salary) => {
+		const newItem = {
+			name,
+			salary,
+			id: this.maxId++,
+			increase: false,
+			like: false,
+		}
+		this.setState(({ data }) => {
+			const newArr = [...data, newItem]; // в старый массив записываем новый обьект
+			return {
+				data: newArr
+			}
+		});
+	}
 
-			<div className="search-panel">
-				<SearchPanel />
-				<AppFilter />
+	render() {
+		return (
+			<div className="app">
+				<AppInfo />
+
+				<div className="search-panel">
+					<SearchPanel />
+					<AppFilter />
+				</div>
+
+				<EmployeesList
+					data={this.state.data}
+					onDelete={this.deleteItem} />
+				<EmployeesAddForm onAddItem={this.addItem} />
 			</div>
-
-			<EmployeesList data={data} />
-			<EmployeesAddForm />
-		</div>
-	);
+		);
+	}
 }
 
 export default App;
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,12 +2,16 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 
 import './employees-list.css';
 
-const EmployeesList = ({data}) => {
+const EmployeesList = ({data, onDelete}) => {
 
     const elements = data.map(item => {
-        const{id, name, salary, increase} = item;
+        const{id, name, salary, increase,} = item;
         return(
-            <EmployeesListItem key={id} name={name} salary={salary} increase={increase}/>
+            <EmployeesListItem 
+            key={id} name={name} 
+            salary={salary} 
+            increase={increase}
+            onDelete={() => onDelete(id)}/>
         )
     })
 
